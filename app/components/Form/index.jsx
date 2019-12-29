3453f40fffe44ecc375059c1db804036871c8369
JoeHetfield <joe_19840729@hotmail.com>
2019-12-30 00:26:36 +0800

    commit

--- app/components/Form/index.jsx
+++ app/components/Form/index.jsx
@@ -1,19 +1,7 @@
-import Radio from './Radio';
-import Slider from './Slider';
-import Checkbox from './Checkbox';
-import FileField from './FileField';
 import TextField from './TextField';
 import SelectField from './SelectField';
-import InputBoxField from './InputBoxField';
-import SelectBoxField from './SelectBoxField';
 
 export {
-  Radio,
-  Slider,
-  Checkbox,
-  FileField,
   TextField,
   SelectField,
-  InputBoxField,
-  SelectBoxField,
 };
